dff5dd0e992e44a47a9b7c26343d9aee4c23408e
Nishit Vataliya <nishitvataliya003@gmail.com>
2024-12-24 09:20:14 +0530

    Reverse the Array

---
+++ ReverseArray.java
@@ -0,0 +1,13 @@
+class ReverseArray {
+    public void reverseArray(int arr[]) {
+        int i=0, j=arr.length-1;
+        
+        while(i<=j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
